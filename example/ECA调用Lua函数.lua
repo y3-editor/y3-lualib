4db1c17149af0ce6e698c66bc95b0d5dc9bb49f8
最萌小汐 <sumneko@hotmail.com>
2023-06-30 19:04:15 +0800

    加个ECA绑定的例子

---
+++ example/ECA调用Lua函数.lua
@@ -0,0 +1,17 @@
+-- 声明一个新的ECA函数，用于获取单位所有者
+New 'ECAFunction' '获取单位所有者'
+    -- 声明第一个参数的ECA类型
+    : with_param('单位', 'py.Unit')
+    -- 声明返回值的ECA类型
+    : with_return('玩家', 'py.Role')
+    ---@param unit Unit
+    ---@return Player
+    : call(function (unit)
+        -- unit 已经是Lua框架的Unit类型
+        local p = unit:get_owner()
+        -- 直接返回Lua框架的Player类型即可
+        return p
+    end)
+
+-- 编辑器中使用ECA “执行Lua代码” 来调用上述函数
+-- 设置变量 玩家 = 执行Lua代码 “Bind['获取单位所有者'](arg[1])”，参数列表： 玩家
