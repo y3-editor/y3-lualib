c6dba24313f5f1a4a359f567ad3b32a515076954
最萌小汐 <sumneko@hotmail.com>
2023-07-03 11:18:59 +0800

    回调注解

--- game/game_event.lua
+++ game/game_event.lua
@@ -5,7 +5,7 @@ local M = Class 'Game'
 M._event_object = {'<GAME_EVENT_OBJECT>'}
 
 ---@param event_type y3.Const.EventType
----@param callback fun()
+---@param callback fun(trg: Trigger, data: table)
 ---@return Trigger
 function M:event(event_type, callback)
     local event_manager = y3.py_event_sub.event_register(M._event_object, event_type)
