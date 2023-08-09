548c2415924224a64652ba02cddd1a451b08d820
最萌小汐 <sumneko@hotmail.com>
2023-08-09 10:03:45 +0800

    不要修改默认的同步设置

--- game/config.lua
+++ game/config.lua
@@ -1,12 +1,19 @@
 ---@class Config
 local M = Class 'Config'
 
----@class Config.Sync
 -- 同步相关的配置，当设置为 `true` 后将启用同步，
 -- 会产生额外的流量。  
 -- 同步需要一定的时间，获取到的是一小段时间前的状态，
 -- 因此启用同步后不能立即获取状态。  
-M.sync = y3.proxy.new({}, {
+---@class Config.Sync
+---@field mouse boolean # 同步玩家的鼠标位置
+---@field key boolean # 同步玩家的键盘与鼠标按键
+---@field camera boolean # 同步玩家的镜头
+M.sync = y3.proxy.new({
+    mouse  = false,
+    key    = false,
+    camera = false,
+}, {
     updateRaw = true,
     setter = {
         mouse = function (self, raw, key, value, config)
@@ -26,12 +33,5 @@ M.sync = y3.proxy.new({}, {
         end,
     },
 })
-
--- 同步玩家的鼠标位置
-M.sync.mouse  = false
--- 同步玩家的键盘与鼠标按键
-M.sync.key    = false
--- 同步玩家的镜头
-M.sync.camera = false
 
 return M
