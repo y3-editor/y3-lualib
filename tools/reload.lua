a72381749933b73e35404d3da359c27e38560547
最萌小汐 <sumneko@hotmail.com>
2023-08-14 18:31:38 +0800

    暂存

--- tools/reload.lua
+++ tools/reload.lua
@@ -9,6 +9,14 @@ M.includedNameMap = {}
 ---@type string[]
 M.includedNames = {}
 
+---@private
+---@type function[]
+M.beforeReloadCallbacks = {}
+
+---@private
+---@type function[]
+M.afterReloadCallbacks = {}
+
 -- 类似于 `require` ，但是会在重载时重新加载文件。
 ---@param name string
 ---@return any
@@ -28,9 +36,41 @@ function M.include(name)
 end
 
 -- 进行重载
----@param logger? fun(str: string)
-function M.reload(logger)
-    logger = logger or print
+function M.reload()
+    for _, callback in ipairs(M.beforeReloadCallbacks) do
+        xpcall(callback, log.error)
+    end
+
+    local includedNames   = M.includedNames
+    local includedNameMap = M.includedNameMap
+    M.includedNames   = {}
+    M.includedNameMap = {}
+    M.beforeReloadCallbacks = {}
+    M.afterReloadCallbacks  = {}
+
+    for _, name in ipairs(includedNames) do
+        package.loaded[name] = nil
+    end
+
+    for _, name in ipairs(includedNames) do
+        M.include(name)
+    end
+
+    for _, callback in ipairs(M.afterReloadCallbacks) do
+        xpcall(callback, log.error)
+    end
+end
+
+-- 注册在重载之前的回调
+---@param callback function
+function M.onBeforeReload(callback)
+    M.beforeReloadCallbacks[#M.beforeReloadCallbacks+1] = callback
+end
+
+-- 注册在重载之后的回调
+---@param callback function
+function M.onAfterReload(callback)
+    M.afterReloadCallbacks[#M.afterReloadCallbacks+1] = callback
 end
 
 return M
