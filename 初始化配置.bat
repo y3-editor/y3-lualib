9b3ba76c19151bd63fb48578c507831ee39e996e
最萌小汐 <sumneko@hotmail.com>
2023-08-23 16:20:13 +0800

    一键配置

---
+++ 初始化配置.bat
@@ -0,0 +1,5 @@
+@echo off
+xcopy /Y "%~dp0演示\项目配置\extensions.json" "%~dp0..\.vscode\extensions.json"
+xcopy /Y "%~dp0演示\项目配置\launch.json" "%~dp0..\.vscode\launch.json"
+xcopy /Y "%~dp0演示\项目配置\luarc.json" "%~dp0..\.luarc.json"
+pause
