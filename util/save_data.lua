8cbb5435bd5217ae6b432343d6c0dc91d0e3fbd6
最萌小汐 <sumneko@hotmail.com>
2023-10-20 18:46:53 +0800

    添加存档模块

--- util/save_data.lua
+++ util/save_data.lua
@@ -75,9 +75,9 @@ end
 ---@return table
 function M.load_table(player, slot, disable_cover)
     if disable_cover then
-        return {}
+        return M.load_table_with_cover_disable(player, slot)
     else
-        return M.load_table_as_cover(player, slot)
+        return M.load_table_with_cover_enable(player, slot)
     end
 end
 
@@ -85,7 +85,7 @@ end
 ---@param player Player
 ---@param slot integer
 ---@return table
-function M.load_table_as_cover(player, slot)
+function M.load_table_with_cover_enable(player, slot)
     local save_data = player.handle:get_save_data_table_value(slot)
     local create_proxy
     local update_delay = 0.1
@@ -111,9 +111,16 @@ function M.load_table_as_cover(player, slot)
             end
             if type(key) ~= 'string'
             and math.type(key) ~= 'integer' then
-                error('表的key必须是字符串或者整数')
+                error('存档的key必须是字符串或者整数')
             end
-            if type(value) == 'table' and y3.proxy.raw(value) then
+            local vtype = type(value)
+            if  vtype ~= 'nil'
+            and vtype ~= 'boolean'
+            and vtype ~= 'number'
+            and vtype ~= 'table' then
+                error('存档的值只能是基础类型或表')
+            end
+            if vtype == 'table' and y3.proxy.raw(value) then
                 value = y3.proxy.raw(value)
             end
             raw[key] = value
@@ -139,6 +146,77 @@ function M.load_table_as_cover(player, slot)
     end
 
     local proxy_save_data = create_proxy(save_data, 0)
+
+    return proxy_save_data
+end
+
+---@private
+---@param player Player
+---@param slot integer
+---@return table
+function M.load_table_with_cover_disable(player, slot)
+    local save_data = player.handle:get_save_data_table_value(slot)
+    local create_proxy
+    local update_delay = 0.1
+    local update_timer
+
+    local function update_save_data()
+        if update_timer then
+            return
+        end
+        update_timer = y3.ltimer.wait(update_delay, function ()
+            update_timer = nil
+            player.handle:upload_save_data()
+            log.info('自动保存存档：', player, slot)
+        end)
+    end
+
+    local function set_value(key, value)
+        player.handle:set_save_table_key_value(slot, key, value, '', '', '')
+        update_save_data()
+    end
+
+    local function get_value(key)
+    ---@diagnostic disable-next-line: param-type-mismatch
+        return player.handle:get_save_table_key_value(slot, key, '', '', nil, '')
+    end
+
+    ---@type Proxy.Config
+    local proxy_config = {
+        anySetter = function (self, raw, key, value, config)
+            if type(key) ~= 'string'
+            and math.type(key) ~= 'integer' then
+                error('表的key必须是字符串或者整数')
+            end
+            local vtype = type(value)
+            if vtype == 'table' then
+                error('禁止覆盖模式下表不能作为存档的值')
+            end
+            if  vtype ~= 'nil'
+            and vtype ~= 'boolean'
+            and vtype ~= 'number' then
+                error('存档的值只能是基础类型')
+            end
+
+            set_value(key, value)
+        end,
+        anyGetter = function (self, raw, key, config)
+            local value = get_value(key)
+
+            return value
+        end
+    }
+
+    function create_proxy(raw)
+        if M.table_cache[raw] then
+            return M.table_cache[raw]
+        end
+        local v = y3.proxy.new(raw, proxy_config)
+        M.table_cache[raw] = v
+        return v
+    end
+
+    local proxy_save_data = create_proxy(save_data)
 
     return proxy_save_data
 end
