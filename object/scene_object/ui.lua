6c81e986059509b0d34f184eaabe2bc3d5235ebb
Xavier(Home) <Xavier_CHN@live.com>
2023-10-24 12:39:45 +0800

    使ui.lua模块的返回值保持一致

--- object/scene_object/ui.lua
+++ object/scene_object/ui.lua
@@ -209,8 +209,10 @@ end
 
 --设置控件9宫格启用
 ---@param switch boolean # 启用/禁用
+---@return self
 function M:set_ui_9_enable(switch)
     GameAPI.set_ui_comp_scale_9_enable(self.player.handle, self.handle, switch)
+    return self
 end
 
 --设置文本字体大小
@@ -890,9 +892,11 @@ end
 ---@param space? number # 间隔帧数
 ---@param start_frame? integer # 起始帧
 ---@param end_frame? integer # 结束帧
+---@return self
 function M:play_ui_sequence(loop, space, start_frame, end_frame)
     ---@diagnostic disable-next-line: param-type-mismatch
     GameAPI.play_ui_comp_sequence(self.player.handle, self.handle, loop or false, space or 0.1, start_frame or 0, end_frame or -1)
+    return self
 end
 
 return M
