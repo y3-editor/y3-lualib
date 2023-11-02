270efaa19ae7f9a197c2dfc26d937b7bd6dd32a5
最萌小汐 <sumneko@hotmail.com>
2023-11-02 15:13:54 +0800

    简化初始化配置

--- 初始化配置.bat
+++ 初始化配置.bat
@@ -1,7 +1,5 @@
 @echo off
-xcopy /Y "%~dp0演示\项目配置\extensions.json" "%~dp0..\.vscode\extensions.json"
-xcopy /Y "%~dp0演示\项目配置\launch.json" "%~dp0..\.vscode\launch.json"
-xcopy /Y "%~dp0演示\项目配置\luarc.json" "%~dp0..\.luarc.json"
-cd "%~dp0/.."
-md log
+md "%~dp0/../.vscode"
+md "%~dp0/../log"
+xcopy /Y "%~dp0演示\项目配置\*" "%~dp0..\.vscode"
 pause
