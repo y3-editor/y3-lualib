bb159d884db8074b1455b305d2e6bf5c6e1e9dd2
最萌小汐 <sumneko@hotmail.com>
2023-11-07 11:45:10 +0800

    更新例子

--- 演示/物编/挂载事件.lua
+++ 演示/物编/挂载事件.lua
@@ -1,10 +1,13 @@
 local M = y3.object.unit[134274912] -- 关羽
 
 function M.on_create(unit)
-    print('单位-创建')
-    unit:bindGC(y3.ltimer.loop(0.1, function ()
+    print('单位-创建：', unit)
+
+    -- 将计时器绑定在单位上，单位移除时计时器会自动销毁
+    unit:bindGC(y3.ltimer.loop(0.5, function ()
+        print('造成伤害！')
         unit:damage {
-            damage = 10,
+            damage = 100,
             target = unit,
             type = '物理',
         }
@@ -12,5 +15,12 @@ function M.on_create(unit)
 end
 
 function M.on_dead(unit)
-    print('单位-死亡')
+    print('单位-死亡：', unit)
+    y3.ltimer.wait(2, function (timer, count)
+        unit:remove()
+    end)
+end
+
+function M.on_remove(unit)
+    print('单位-移除：', unit)
 end
