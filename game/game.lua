b5f291cf79b9f337bd6f6fc2f860e22d8016faa3
会做饭的二哈 <baikaiwen12@outlook.com>
2024-01-19 12:40:22 +0800

    Update game.lua
    缺少触发器有的方法:
        -- 基础:获取/技能/魔法/物品/单位/的图标方法.
    get_icon_id :
        -- 这个方法似乎可以不再使用了

--- game/game.lua
+++ game/game.lua
@@ -265,8 +265,22 @@ end
 ---根据图片ID获取图片
 ---@param id integer
 ---@return py.Texture texture
+---@deprecated 请直接使用图片ID
 function M.get_icon_id(id)
-    return GameAPI.get_texture_by_icon_id(id)
+    ---@type py.Texture
+    return id
+end
+
+---获取任意对象图片
+---@param obj ?Unit|Item|Ability|Buff 单位|物品|技能|魔法效果
+---@return py.Texture texture
+function M.get_obj_icon(obj)
+    if not obj then
+        -- 如果为空返回空图片
+        return 999
+    end
+    ---@type py.Texture
+    return GameAPI.get_icon_id(obj.handle)
 end
 
 ---设置某点的地形纹理
