060f13923ea192e730e605b56a569882003e0e6d
最萌小汐 <sumneko@hotmail.com>
2024-02-05 16:50:17 +0800

    先规避定点数bug

--- tools/log.lua
+++ tools/log.lua
@@ -132,7 +132,8 @@ end
 ---@return string
 function M:getTimeStamp()
     local deltaClock = self.clock() - self.startClock
-    local sec, ms = math.modf(self.startTime + deltaClock)
+    local deltaSec, ms = math.modf(deltaClock)
+    local sec = self.startTime + deltaSec
     local timeStamp = os.date('%m-%d %H:%M:%S', sec) --[[@as string]]
     timeStamp = ('%s.%03.f'):format(timeStamp, ms * 1000)
     return timeStamp
