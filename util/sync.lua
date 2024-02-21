62155e7134bf740c324555efedbda0d32834f915
ChNopx <389004294@qq.com>
2024-02-21 11:13:14 +0800

    更新 sync.lua

--- util/sync.lua
+++ util/sync.lua
@@ -1,11 +1,11 @@
 -- 将本地数据同步给所有玩家
 ---@class Sync
-local M = Class 'Sync'
+local M = Class "Sync"
 
 ---@private
 M.syncMap = {}
 
--- 发送本地的信息，使用 `onSync` 来同步接受数据  
+-- 发送本地的信息，使用 `onSync` 来同步接受数据
 -- 请在本地环境中使用此函数
 ---@param id string # 以 `$` 开头的 id 保留为内部使用
 ---@param data Serialization.SupportTypes
@@ -14,7 +14,7 @@ function M.send(id, data)
     broadcast_lua_msg(id, bin)
 end
 
--- 同步接收数据，回调函数在同步后执行  
+-- 同步接收数据，回调函数在同步后执行
 -- 同一个 id 只能注册一个回调函数，后注册的会覆盖前面的
 ---@param id string
 ---@param callback fun(data: Serialization.SupportTypes)
@@ -22,7 +22,7 @@ function M.onSync(id, callback)
     M.syncMap[id] = callback
 end
 
-y3.game:event('游戏-接收广播信息', function (trg, data)
+y3.游戏:事件("游戏-接收广播信息", function(trg, data)
     local id = data.broadcast_lua_msg_id
     local callback = M.syncMap[id]
     if not callback then
