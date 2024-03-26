2974e4d2d287162a4f00a3bb712b1f640f09f683
最萌小汐 <sumneko@hotmail.com>
2024-03-26 18:28:26 +0800

    UI要延迟加载

--- 演示/demo/主控UI/init.lua
+++ 演示/demo/主控UI/init.lua
@@ -1,2 +1,4 @@
-require 'y3.演示.demo.主控UI.main_ui'
-require 'y3.演示.demo.主控UI.tips_ui'
+y3.ltimer.wait(0, function()
+    require 'y3.演示.demo.主控UI.main_ui'
+    require 'y3.演示.demo.主控UI.tips_ui'
+end)
