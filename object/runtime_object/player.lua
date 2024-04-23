b02cb9f67bacedbca37c10ebcd1805dbaf9e504a
会做饭的二哈 <baikaiwen12@outlook.com>
2024-04-23 17:08:12 +0800

    Update player.lua
    可选修复, GameAPI.get_role_platform_icon 如果收到离线的玩家会导致python报错,所以增加检测玩家状态是否在线才返回

--- object/runtime_object/player.lua
+++ object/runtime_object/player.lua
@@ -409,7 +409,7 @@ end
 ---获取玩家平台头像
 ---@return integer icon 平台头像
 function M:get_platform_icon()
-    return GameAPI.get_role_platform_icon(self.handle)
+    return self.phandle:get_role_status() == 1 and GameAPI.get_role_platform_icon(self.handle) or 999
 end
 
 ---玩家平台道具数量
