17e6e88473d7d60a7bc2ac9baf8920358cc1bf32
最萌小汐 <sumneko@hotmail.com>
2024-04-29 17:26:22 +0800

    调用ECA自定义函数

--- util/eca_helper.lua
+++ util/eca_helper.lua
@@ -1,5 +1,5 @@
 ---@class ECAHelper
-local M = {}
+local M = Class 'ECAHelper'
 
 --注册ECA函数
 --
@@ -8,6 +8,25 @@ local M = {}
 ---@return ECAFunction
 function M.def(name)
     return New 'ECAFunction' (name)
+end
+
+---@private
+M._call_impls = {}
+
+--调用ECA中定义的自定义事件
+function M.call(...)
+    local impl = M._call_impls[...]
+    if not impl then
+        error('不存在此自定义事件：' .. tostring(...))
+    end
+    impl(...)
+end
+
+---@private
+---@param name string
+---@param impl function
+function M.register_custom_event_impl(name, impl)
+    M._call_impls[name] = impl
 end
 
 return M
