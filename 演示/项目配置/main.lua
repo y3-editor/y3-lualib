f3b88b63e1ac3958d71beec5b8e6e5a257646bfb
最萌小汐 <sumneko@hotmail.com>
2024-04-29 10:34:53 +0800

    更新默认项目配置

--- 演示/项目配置/main.lua
+++ 演示/项目配置/main.lua
@@ -1,5 +1,7 @@
 -- 游戏启动后会自动运行此文件
-y3.config.log.toGame = true
+
+--在开发模式下，将日志打印到游戏中
+y3.config.log.toGame = y3.game.is_debug_mode()
 
 y3.game:event('游戏-初始化', function (trg, data)
     print('Hello, Y3!')
