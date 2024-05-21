1cd921072121d1b2a0f2ecd326e58648e0d8b00e
sunbingzhou <642677324@qq.com>
2024-05-21 12:11:38 +0800

    feat: object.item 添加进入物品栏/背包的监听

--- util/object.lua
+++ util/object.lua
@@ -7,7 +7,7 @@ local M = Class 'EditorObject'
 local DataModule = Class 'EditorObject.DataModule'
 
 ---@diagnostic disable-next-line: undefined-field
-DataModule.__getter.data = function (self)
+DataModule.__getter.data = function(self)
     return GameAPI.api_get_editor_type_data(self.data_key, self.key), true
 end
 
@@ -16,8 +16,8 @@ end
 ---@field on_create? fun(unit: Unit) # 单位创建后执行
 ---@field on_remove? fun(unit: Unit) # 单位移除后执行
 ---@field on_dead? fun(unit: Unit) # 单位死亡后执行
---单位的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）  
---> 警告：请确保数据类型正确，否则可能导致崩溃  
+--单位的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）
+--> 警告：请确保数据类型正确，否则可能导致崩溃
 --> 警告：如果创建过此单位再修改数据，行为是未定义的
 ---@field data Object.Unit
 local Unit = Class 'EditorObject.Unit'
@@ -42,7 +42,7 @@ function Unit:new()
 end
 
 ---@type table<integer, EditorObject.Unit>
-M.unit = y3.util.defaultTable(function (key)
+M.unit = y3.util.defaultTable(function(key)
     return New 'EditorObject.Unit' (key)
 end)
 
@@ -52,8 +52,10 @@ end)
 ---@field on_lose? fun(item: Item) # 物品失去后执行
 ---@field on_create? fun(item: Item) # 物品创建后执行
 ---@field on_remove? fun(item: Item) # 物品移除后执行
---物品的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）  
---> 警告：请确保数据类型正确，否则可能导致崩溃  
+---@field on_add_to_pkg? fun(item: Item) # 物品进入背包后执行
+---@field on_add_to_bar? fun(item: Item) # 物品进入装备栏后执行
+--物品的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）
+--> 警告：请确保数据类型正确，否则可能导致崩溃
 --> 警告：如果创建过此物品再修改数据，行为是未定义的
 ---@field data Object.Item
 local Item = Class 'EditorObject.Item'
@@ -78,7 +80,7 @@ function Item:new()
 end
 
 ---@type table<integer, EditorObject.Item>
-M.item = y3.util.defaultTable(function (key)
+M.item = y3.util.defaultTable(function(key)
     return New 'EditorObject.Item' (key)
 end)
 
@@ -89,8 +91,8 @@ end)
 ---@field on_lose? fun(buff: Buff) # 效果失去后执行
 ---@field on_pulse? fun(buff: Buff) # 效果心跳后执行
 ---@field on_stack_change? fun(buff: Buff) # 效果层数变化后执行
---魔法效果的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）  
---> 警告：请确保数据类型正确，否则可能导致崩溃  
+--魔法效果的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）
+--> 警告：请确保数据类型正确，否则可能导致崩溃
 --> 警告：如果创建过此魔法效果再修改数据，行为是未定义的
 ---@field data Object.Buff
 local Buff = Class 'EditorObject.Buff'
@@ -115,7 +117,7 @@ function Buff:new()
 end
 
 ---@type table<integer, EditorObject.Buff>
-M.buff = y3.util.defaultTable(function (key)
+M.buff = y3.util.defaultTable(function(key)
     return New 'EditorObject.Buff' (key)
 end)
 
@@ -131,8 +133,8 @@ end)
 ---@field on_cast_shot? fun(ability: Ability, cast: Cast) # 技能出手施法时执行
 ---@field on_cast_finish? fun(ability: Ability, cast: Cast) # 技能完成施法时执行
 ---@field on_cast_stop? fun(ability: Ability, cast: Cast) # 技能停止施法时执行
---技能的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）  
---> 警告：请确保数据类型正确，否则可能导致崩溃  
+--技能的物编数据，你可以从里面读取或修改任意物编（部分字段无法修改）
+--> 警告：请确保数据类型正确，否则可能导致崩溃
 --> 警告：如果创建过此技能再修改数据，行为是未定义的
 ---@field data Object.Ability
 local Ability = Class 'EditorObject.Ability'
@@ -157,20 +159,20 @@ function Ability:new()
 end
 
 ---@type table<integer, EditorObject.Ability>
-M.ability = y3.util.defaultTable(function (key)
+M.ability = y3.util.defaultTable(function(key)
     return New 'EditorObject.Ability' (key)
 end)
 
 M.lock_count_map = setmetatable({}, {
     __mode = 'k',
-    __index = function (t, k)
+    __index = function(t, k)
         t[k] = 0
         return 0
     end,
 })
 M.call_stack_map = setmetatable({}, {
     __mode = 'k',
-    __index = function (t, k)
+    __index = function(t, k)
         t[k] = {}
         return t[k]
     end,
@@ -203,7 +205,7 @@ function M.callMethod(otype, mname, key, lock_obj, arg1, arg2)
     end
     local stack = M.call_stack_map[lock_obj]
     if M.lock_count_map[key] > 0 then
-        stack[#stack+1] = function ()
+        stack[#stack + 1] = function()
             applyMethod(stack, key, func, arg1, arg2)
         end
         return
@@ -213,7 +215,7 @@ end
 
 local function subscribe(class, method, callback)
     local mark
-    class.__setter[method] = function (self, value)
+    class.__setter[method] = function(self, value)
         if not mark then
             mark = true
             callback()
@@ -222,134 +224,146 @@ local function subscribe(class, method, callback)
     end
 end
 
-subscribe(Unit, 'on_create', function ()
-    y3.game:event('单位-创建', function (trg, data)
+subscribe(Unit, 'on_create', function()
+    y3.game:event('单位-创建', function(trg, data)
         M.callMethod('unit', 'on_create', data.unit:get_key(), data.unit, data.unit)
     end)
 end)
 
-subscribe(Unit, 'on_remove', function ()
-    y3.game:event('单位-移除', function (trg, data)
+subscribe(Unit, 'on_remove', function()
+    y3.game:event('单位-移除', function(trg, data)
         M.callMethod('unit', 'on_remove', data.unit:get_key(), data.unit, data.unit)
     end)
 end)
 
-subscribe(Unit, 'on_dead', function ()
-    y3.game:event('单位-死亡', function (trg, data)
+subscribe(Unit, 'on_dead', function()
+    y3.game:event('单位-死亡', function(trg, data)
         M.callMethod('unit', 'on_dead', data.unit:get_key(), data.unit, data.unit)
     end)
 end)
 
-subscribe(Item, 'on_add', function ()
-    y3.game:event('物品-获得', function (trg, data)
+subscribe(Item, 'on_add', function()
+    y3.game:event('物品-获得', function(trg, data)
         M.callMethod('item', 'on_add', data.item:get_key(), data.item, data.item)
     end)
 end)
 
-subscribe(Item, 'on_lose', function ()
-    y3.game:event('物品-失去', function (trg, data)
+subscribe(Item, 'on_add_to_pkg', function()
+    y3.game:event('物品-进入背包', function(trg, data)
+        M.callMethod('item', 'on_add_to_pkg', data.item:get_key(), data.item, data.item)
+    end)
+end)
+
+subscribe(Item, 'on_add_to_bar', function()
+    y3.game:event('物品-进入物品栏', function(trg, data)
+        M.callMethod('item', 'on_add_to_bar', data.item:get_key(), data.item, data.item)
+    end)
+end)
+
+subscribe(Item, 'on_lose', function()
+    y3.game:event('物品-失去', function(trg, data)
         M.callMethod('item', 'on_lose', data.item:get_key(), data.item, data.item)
     end)
 end)
 
-subscribe(Item, 'on_create', function ()
-    y3.game:event('物品-创建', function (trg, data)
+subscribe(Item, 'on_create', function()
+    y3.game:event('物品-创建', function(trg, data)
         M.callMethod('item', 'on_create', data.item:get_key(), data.item, data.item)
     end)
 end)
 
-subscribe(Item, 'on_remove', function ()
-    y3.game:event('物品-移除', function (trg, data)
+subscribe(Item, 'on_remove', function()
+    y3.game:event('物品-移除', function(trg, data)
         M.callMethod('item', 'on_remove', data.item:get_key(), data.item, data.item)
     end)
 end)
 
-subscribe(Buff, 'on_can_add', function ()
-    y3.game:event('效果-即将获得', function (trg, data)
+subscribe(Buff, 'on_can_add', function()
+    y3.game:event('效果-即将获得', function(trg, data)
         M.callMethod('buff', 'on_can_add', data.buff:get_key(), data.buff, data.buff)
     end)
 end)
 
-subscribe(Buff, 'on_add', function ()
-    y3.game:event('效果-获得', function (trg, data)
+subscribe(Buff, 'on_add', function()
+    y3.game:event('效果-获得', function(trg, data)
         M.callMethod('buff', 'on_add', data.buff:get_key(), data.buff, data.buff)
     end)
 end)
 
-subscribe(Buff, 'on_lose', function ()
-    y3.game:event('效果-失去', function (trg, data)
+subscribe(Buff, 'on_lose', function()
+    y3.game:event('效果-失去', function(trg, data)
         M.callMethod('buff', 'on_lose', data.buff:get_key(), data.buff, data.buff)
     end)
 end)
 
-subscribe(Buff, 'on_pulse', function ()
-    y3.game:event('效果-心跳', function (trg, data)
+subscribe(Buff, 'on_pulse', function()
+    y3.game:event('效果-心跳', function(trg, data)
         M.callMethod('buff', 'on_pulse', data.buff:get_key(), data.buff, data.buff)
     end)
 end)
 
-subscribe(Buff, 'on_stack_change', function ()
-    y3.game:event('效果-层数变化', function (trg, data)
+subscribe(Buff, 'on_stack_change', function()
+    y3.game:event('效果-层数变化', function(trg, data)
         M.callMethod('buff', 'on_stack_change', data.buff:get_key(), data.buff, data.buff)
     end)
 end)
 
-subscribe(Ability, 'on_add', function ()
-    y3.game:event('技能-获得', function (trg, data)
+subscribe(Ability, 'on_add', function()
+    y3.game:event('技能-获得', function(trg, data)
         M.callMethod('ability', 'on_add', data.ability:get_key(), data.ability, data.ability)
     end)
 end)
 
-subscribe(Ability, 'on_lose', function ()
-    y3.game:event('技能-失去', function (trg, data)
+subscribe(Ability, 'on_lose', function()
+    y3.game:event('技能-失去', function(trg, data)
         M.callMethod('ability', 'on_lose', data.ability:get_key(), data.ability, data.ability)
     end)
 end)
 
-subscribe(Ability, 'on_cooldown', function ()
-    y3.game:event('技能-冷却结束', function (trg, data)
+subscribe(Ability, 'on_cooldown', function()
+    y3.game:event('技能-冷却结束', function(trg, data)
         M.callMethod('ability', 'on_cooldown', data.ability:get_key(), data.ability, data.ability)
     end)
 end)
 
-subscribe(Ability, 'on_upgrade', function ()
-    y3.game:event('技能-升级', function (trg, data)
+subscribe(Ability, 'on_upgrade', function()
+    y3.game:event('技能-升级', function(trg, data)
         M.callMethod('ability', 'on_upgrade', data.ability:get_key(), data.ability, data.ability)
     end)
 end)
 
-subscribe(Ability, 'on_can_cast', function ()
-    y3.game:event('施法-即将开始', function (trg, data)
+subscribe(Ability, 'on_can_cast', function()
+    y3.game:event('施法-即将开始', function(trg, data)
         M.callMethod('ability', 'on_can_cast', data.ability:get_key(), nil, data.ability, data.cast)
     end)
 end)
 
-subscribe(Ability, 'on_cast_start', function ()
-    y3.game:event('施法-开始', function (trg, data)
+subscribe(Ability, 'on_cast_start', function()
+    y3.game:event('施法-开始', function(trg, data)
         M.callMethod('ability', 'on_cast_start', data.ability:get_key(), nil, data.ability, data.cast)
     end)
 end)
 
-subscribe(Ability, 'on_cast_channel', function ()
-    y3.game:event('施法-引导', function (trg, data)
+subscribe(Ability, 'on_cast_channel', function()
+    y3.game:event('施法-引导', function(trg, data)
         M.callMethod('ability', 'on_cast_channel', data.ability:get_key(), nil, data.ability, data.cast)
     end)
 end)
 
-subscribe(Ability, 'on_cast_shot', function ()
-    y3.game:event('施法-出手', function (trg, data)
+subscribe(Ability, 'on_cast_shot', function()
+    y3.game:event('施法-出手', function(trg, data)
         M.callMethod('ability', 'on_cast_shot', data.ability:get_key(), nil, data.ability, data.cast)
     end)
 end)
 
-subscribe(Ability, 'on_cast_finish', function ()
-    y3.game:event('施法-完成', function (trg, data)
+subscribe(Ability, 'on_cast_finish', function()
+    y3.game:event('施法-完成', function(trg, data)
         M.callMethod('ability', 'on_cast_finish', data.ability:get_key(), nil, data.ability, data.cast)
     end)
 end)
 
-subscribe(Ability, 'on_cast_stop', function ()
-    y3.game:event('施法-停止', function (trg, data)
+subscribe(Ability, 'on_cast_stop', function()
+    y3.game:event('施法-停止', function(trg, data)
         M.callMethod('ability', 'on_cast_stop', data.ability:get_key(), nil, data.ability, data.cast)
     end)
 end)
