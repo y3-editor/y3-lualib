a62288fa8f08c58fa09c601cae82fdd0ae6b8a49
最萌小汐 <sumneko@hotmail.com>
2024-06-19 15:47:50 +0800

    先自己把来源带在数据包里

--- util/sync.lua
+++ util/sync.lua
@@ -10,7 +10,12 @@ M.syncMap = {}
 ---@param id string # 以 `$` 开头的 id 保留为内部使用
 ---@param data Serialization.SupportTypes
 function M.send(id, data)
-    local bin = y3.dump.encode(data)
+    --TODO 先把来源玩家放到数据里传过去
+    local bin = y3.base64.encode(y3.dump.encode({
+        data = data,
+        ---@diagnostic disable-next-line: deprecated
+        source = y3.player.get_local()
+    }))
     broadcast_lua_msg(id, bin)
 end
 
@@ -28,11 +33,11 @@ y3.game:event('游戏-接收广播信息', function (trg, data)
     if not callback then
         return
     end
-    local suc, value = pcall(y3.dump.decode, data.broadcast_lua_msg_content)
+    local suc, value = pcall(y3.dump.decode, y3.base64.decode(data.broadcast_lua_msg_content))
     if not suc then
         return
     end
-    xpcall(callback, log.error, value, data.player)
+    xpcall(callback, log.error, value.data, value.source)
 end)
 
 return M
