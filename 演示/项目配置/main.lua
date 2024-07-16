3d251a6f0bda3b481b02c3419dc6311e5bdc6fe2
最萌小汐 <sumneko@hotmail.com>
2024-07-16 17:31:49 +0800

    修改日志的默认设置

--- 演示/项目配置/main.lua
+++ 演示/项目配置/main.lua
@@ -1,7 +1,13 @@
 -- 游戏启动后会自动运行此文件
 
 --在开发模式下，将日志打印到游戏中
-y3.config.log.toGame = y3.game.is_debug_mode()
+if y3.game.is_debug_mode() then
+    y3.config.log.toGame = true
+    y3.config.log.level  = 'debug'
+else
+    y3.config.log.toGame = false
+    y3.config.log.level  = 'info'
+end
 
 y3.game:event('游戏-初始化', function (trg, data)
     print('Hello, Y3!')
