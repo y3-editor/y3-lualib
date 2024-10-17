04f007179eacf7ad7e66f42efb3516388b0c521d
最萌小汐 <sumneko@hotmail.com>
2024-10-17 14:44:48 +0800

    这个不用改

--- game/game.lua
+++ game/game.lua
@@ -859,7 +859,7 @@ end
 ---@return number x, number y
 function M.world_pos_to_screen_edge_pos(world_pos, delta_dis)
     ---@diagnostic disable-next-line: param-type-mismatch
-    local pos = GameAPI.api_world_pos_to_screen_edge_pos(world_pos.handle, Fix32(delta_dis))
+    local pos = GameAPI.api_world_pos_to_screen_edge_pos(world_pos.handle, delta_dis)
     ---@diagnostic disable-next-line: param-type-mismatch
     local x = GlobalAPI.get_fixed_coord_index(pos, 0):float() / 100
     ---@diagnostic disable-next-line: param-type-mismatch
