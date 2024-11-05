82b09b20c8813c93aa94a0bd470acddfaefae288
最萌小汐 <sumneko@hotmail.com>
2024-11-05 16:01:42 +0800

    挂载在玩家上的界面事件从全局事件转发

--- util/object.lua
+++ util/object.lua
@@ -1,5 +1,5 @@
 local event_configs = require 'y3.meta.eventconfig'
-local event_datas   = require 'y3.meta.event'
+local get_master    = require 'y3.util.get_master'
 
 -- 物体编辑器
 ---@class EditorObject
@@ -21,29 +21,6 @@ local Event = Class 'EditorObject.Event'
 
 local gameEvents = {}
 
-local function get_master(datas, config, lua_params)
-    local master = config.master_data
-    if not master then
-        if config.master then
-            for _, data in ipairs(datas) do
-                if data.lua_name == config.master then
-                    master = data
-                    break
-                end
-            end
-        else
-            for _, data in ipairs(datas) do
-                if data.lua_type == config.object then
-                    master = data
-                    break
-                end
-            end
-        end
-        config.master_data = master
-    end
-    return lua_params[master.lua_name]
-end
-
 ---@param self EditorObject.Event
 ---@param name string
 ---@return Trigger
@@ -54,9 +31,8 @@ local function initGameEventProxy(self, name)
     if not config or not config.object then
         error(string.format('事件“%s”无法作为对象事件', name))
     end
-    local datas = event_datas[config.key]
     return y3.game:event(name, function (trg, data)
-        local master = get_master(datas, config, data)
+        local master = get_master(name, data)
         if not master then
             return
         end
