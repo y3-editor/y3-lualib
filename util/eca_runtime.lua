976db55bea0237040ea9f61668c086f78d8d982d
最萌小汐 <sumneko@hotmail.com>
2024-12-20 18:54:23 +0800

    更新

--- util/eca_runtime.lua
+++ util/eca_runtime.lua
@@ -21,26 +21,40 @@ function M.evaluate(code, ...)
     return f(...)
 end
 
-local array_meta_map1 = setmetatable({}, { __index = function (t, default)
-    local mt = { __index = function (array, k)
-        array[k] = default
-        return default
-    end }
+local array_meta = { __index = function (t, k)
+    if k == nil then
+        return nil
+    end
+    local v = t._default
+    t[k] = v
+    return v
+end }
 
+local array_meta_map = setmetatable({}, { __index = function (t, default)
+    local mt
+    if type(default) == 'string' and #default > 0 then
+        local make_default = assert(load('return ' .. default))
+        local tv = make_default()
+        local tp = type(tv)
+        if tp == 'nil' or tp == 'number' or tp == 'boolean' or tp == 'string' then
+            mt = { __index = function (array, k)
+                array[k] = tv
+                return tv
+            end }
+        else
+            mt = { __index = function (array, k)
+                local v = make_default()
+                array[k] = v
+                return v
+            end }
+        end
+    else
+        mt = { __index = function (array, k)
+            array[k] = default
+            return default
+        end }
+    end
     t[default] = mt
-    return mt
-end })
-
-local array_meta_map2 = setmetatable({}, { __index = function (t, code)
-    local make_default = assert(load('return ' .. code))
-
-    local mt = { __index = function (array, k)
-        local v = make_default()
-        array[k] = v
-        return v
-    end }
-
-    t[code] = mt
     return mt
 end })
 
@@ -48,11 +62,7 @@ function M.array(default)
     if default == nil then
         return {}
     end
-    return setmetatable({}, array_meta_map1[default])
-end
-
-function M.array_code(code)
-    return setmetatable({}, array_meta_map2[code])
+    return setmetatable({}, array_meta_map[default])
 end
 
 return M
