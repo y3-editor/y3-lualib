5eb5d824679964cb6124ce6e38d3e202c503de2b
最萌小汐 <sumneko@hotmail.com>
2025-02-07 15:16:00 +0800

    修正施法的泄露

--- object/runtime_object/cast.lua
+++ object/runtime_object/cast.lua
@@ -94,16 +94,19 @@ end
 function M.get(ability, cast_id)
     if not ability._castMap then
         ability._castMap = {}
+        ability:event('施法-结束', function (trg, data)
+            local id = data.cast.cast_id
+            local cast = ability._castMap[id]
+            if cast then
+                y3.ltimer.wait(5, function ()
+                    ability._castMap[id] = nil
+                end)
+            end
+        end)
     end
     if not ability._castMap[cast_id] then
         local cast = New 'Cast' (ability, cast_id)
         ability._castMap[cast_id] = cast
-        ability:event('施法-结束', function (trg, data)
-            local id = data.cast.cast_id
-            if id == cast_id then
-                Delete(cast)
-            end
-        end)
     end
     return ability._castMap[cast_id]
 end
