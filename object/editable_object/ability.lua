aed7eed04aac089e20f3b58540fea6ddb973dd04
最萌小汐 <sumneko@hotmail.com>
2025-09-11 17:24:33 +0800

    加一点缓存

--- object/editable_object/ability.lua
+++ object/editable_object/ability.lua
@@ -80,8 +80,13 @@ y3.py_converter.register_lua_to_py('py.Ability', function (lua_value)
     return lua_value.handle
 end)
 
+---@package
+M._cache_key = nil
 function M:get_key()
-    return self.handle:api_get_ability_id()
+    if not self._cache_key then
+        self._cache_key = self.handle:api_get_ability_id()
+    end
+    return self._cache_key
 end
 
 ---是否受冷却缩减影响
@@ -204,8 +209,14 @@ function M:set_stack(value)
     self.handle:api_set_ability_stack_count(value)
 end
 
+---@package
+M._cache_name = nil
+
 function M:get_name()
-    return self.handle:api_get_name()
+    if not self._cache_name then
+        self._cache_name = self.handle:api_get_name()
+    end
+    return self._cache_name
 end
 
 ---设置实数属性
