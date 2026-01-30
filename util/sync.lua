724070ec991d2d065a52f5c4792b53a46637ae8c
最萌小汐 <sumneko@hotmail.com>
2026-01-30 18:38:26 +0800

    更新 sync 库

--- util/sync.lua
+++ util/sync.lua
@@ -5,19 +5,35 @@ local M = Class 'Sync'
 ---@private
 M.syncMap = {}
 
+M.syncLocalCount = 0
+M.syncLocalCallbackMap = {}
+
 -- 发送本地的信息，使用 `onSync` 来同步接受数据  
 -- 请在本地环境中使用此函数
+---@generic T: Serialization.SupportTypes
 ---@param id string # 以 `$` 开头的 id 保留为内部使用
----@param data Serialization.SupportTypes
-function M.send(id, data)
-    local bin = y3.dump.encode(data)
-    broadcast_lua_msg(id, bin)
+---@param data T # 如果包含对象，需要在类上实现 `__encode` 和 `__decode` 方法
+---@param done? async fun(data: T)
+function M.send(id, data, done)
+    if done then
+        M.syncLocalCount = M.syncLocalCount + 1
+        M.syncLocalCallbackMap[M.syncLocalCount] = done
+        local bin = y3.dump.encode({
+            id    = id,
+            count = M.syncLocalCount,
+            value = data,
+        })
+        broadcast_lua_msg('$sync_proxy', bin)
+    else
+        local bin = y3.dump.encode(data)
+        broadcast_lua_msg(id, bin)
+    end
 end
 
 -- 同步接收数据，回调函数在同步后执行  
 -- 同一个 id 只能注册一个回调函数，后注册的会覆盖前面的
 ---@param id string
----@param callback fun(data: Serialization.SupportTypes, source: Player)
+---@param callback async fun(data: Serialization.SupportTypes, source: Player)
 function M.onSync(id, callback)
     M.syncMap[id] = callback
 end
@@ -32,7 +48,33 @@ y3.game:event('游戏-接收广播信息', function (trg, data)
     if not suc then
         return
     end
-    xpcall(callback, log.error, value, data.player)
+    y3.await.call(function ()
+        xpcall(callback, log.error, value, data.player)
+    end)
+end)
+
+---@param data table
+---@param source Player
+M.onSync('$sync_proxy', function (data, source)
+    y3.await.call(function ()
+        local id    = data.id
+        local count = data.count
+        local value = data.value
+        local callback = M.syncMap[id]
+        if callback then
+            xpcall(callback, log.error, value, source)
+        end
+        y3.player.with_local(function (local_player)
+            if source ~= local_player then
+                return
+            end
+            local done = M.syncLocalCallbackMap[count]
+            M.syncLocalCallbackMap[count] = nil
+            if done then
+                xpcall(done, log.error, value)
+            end
+        end)
+    end)
 end)
 
 return M
